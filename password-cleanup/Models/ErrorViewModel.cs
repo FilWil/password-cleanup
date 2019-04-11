3932a436cc08f42c30202e24095d3791b8ba7027
Filip Wilczura <filipwilczura@gmail.com>
2019-04-11 18:09:37 +0200

    Add project files.

---
+++ password-cleanup/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace password_cleanup.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
